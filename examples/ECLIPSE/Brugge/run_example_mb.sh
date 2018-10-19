399fcce1cb2fc05e24125358a20b2f6a2d30056b
bellout <mathias.bellout@ntnu.no>
2018-10-19 19:36:54 +0200

    Update run_example_mb + add fieldopt-output to gitignore.

--- examples/ECLIPSE/Brugge/run_example_mb.sh
+++ examples/ECLIPSE/Brugge/run_example_mb.sh
@@ -19,9 +19,10 @@
 #   FIELDOPT_OUT - Path to the output directory to use.
 
 clear 
-# =========================================================
+# ====================================================
 printf '=%.0s' {1..100}; printf '\n' " "
 
+# ----------------------------------------------------
 declare -a CASES=(
 	# orig jsons: 1-5
     "fo_driver.controls.cs"              # 1 
@@ -33,17 +34,19 @@ declare -a CASES=(
     # dbg jsons:
     "fo_driver.controls.cs.dbg"              #  7 [SER=OK] [MPI=OK]
     "fo_driver.curved_wells.controls.cs.dbg" #  8 [SER=OK] [MPI=OK]
-    "fo_driver.control.npv.apps.dbg"         #  9 [SER=OK, add ecl] [MPI=FAIL]
-    "fo_driver.control.npv.cs.dbg"           # 10 [SER=OK, add ecl] [MPI=FAIL]
-    "fo_driver.control.rate.npv.apps.dbg"    # 11 [SER=OK] [MPI=FAIL]
-    "fo_driver.compartments.npv.apps.dbg"    # 12 [SER=OK] [MPI=FAIL]
+    "fo_driver.control.npv.apps.dbg"         #  9 [SER=OK] [MPI=OK]
+    "fo_driver.control.npv.cs.dbg"           # 10 [SER=OK] [MPI=OK]
+    "fo_driver.control.rate.npv.apps.dbg"    # 11 [SER=OK] [MPI=OK]
+    "fo_driver.compartments.npv.apps.dbg"    # 12 [SER=OK] [MPI=OK]
 )
 
+# ----------------------------------------------------
 for (( i=1; i<${#CASES[@]}+1; i++ ))
 do
 printf 'Case %02.0f: %s\n' ${i} "${CASES[i-1]}"
 done
 
+# ----------------------------------------------------
 FODIRTLS="${HOME}/projects/PetroleumCyberneticsGroup/FieldOpt-Research"
 FODIRMB="${HOME}/git/IOC/FieldOpt-Research"
 FODIR=${FODIRMB}
@@ -53,7 +56,7 @@ FIELDOPT_BIN=${FODIR}/FieldOpt/cmake-build-debug/bin/FieldOpt
 FIELDOPT_OUT=${FODIR}/fieldopt-output
 # FIELDOPT_OUT=/home/bellout/git/IOC/FieldOpt-Research/examples/ECLIPSE/fieldopt-output
 
-# =========================================================
+# ====================================================
 printf '=%.0s' {1..100}; printf '\n' " "
 
 # Set case and path variables
@@ -71,14 +74,14 @@ SCR_PATH=${FIELDOPT_BUILD}/execution_scripts/bash_ecl.sh
 printf '%s\n' "DBG: rm -rfv ${OUTPUT_DIR}:" 
 rm -rfv ${OUTPUT_DIR}
 
-# =========================================================
+# ====================================================
 printf '=%.0s' {1..100}; printf '\n' " "
 
 # Re-create ouput subdirectory
 printf '%s\n' "DBG: mkdir -v ${OUTPUT_DIR}:" 
 mkdir -v ${OUTPUT_DIR}
 
-# =========================================================
+# ====================================================
 printf '=%.0s' {1..100}; printf '\n%s\n' "Paths:"
 
 # Print paths
@@ -87,7 +90,7 @@ printf '%s\n' "Driver: ${DRIVER_PATH}"
 printf '%s\n' "Deck:   ${DECK_PATH}"
 printf '%s\n' "Grid:   ${GRID_PATH}"
 
-# =========================================================
+# ====================================================
 printf '=%.0s' {1..100}; printf '\n' " "
 
 # Set LD_LIBRARY_PATH
@@ -96,31 +99,23 @@ LDPATH="LD_LIBRARY_PATH=${BHOME}\
 
 # Make run command (MPI)
 export ${LDPATH}
-CMDMPI="mpirun -np 3 -mca btl ^openib -vv ${FIELDOPT_BIN} "\
+CMDMPI="mpirun -np 3 -mca btl ^openib ${FIELDOPT_BIN} "\
 "${DRIVER_PATH} ${OUTPUT_DIR} -r mpisync -n1 -m3 "\
 "-g ${GRID_PATH} -s ${DECK_PATH} "\
 "-e ${SCR_PATH} -f -v 3 -t 0" 
 
-# old
-# CMDMPI="mpirun -n 5 \"${LDPATH} ${FIELDOPT_BIN} \
-# ${DRIVER_PATH} ${OUTPUT_DIR} -r mpisync -n1 \
-# -m4 -t5 --force -g ${GRID_PATH} -s ${DECK_PATH} \
-# -e ${SCR_PATH} -v3\" "
+printf '%s\n' "CMDMPI=${CMDMPI}"
 
-echo "CMDMPI=${CMDMPI}"
-echo ""
-
-# =========================================================
+# ====================================================
 printf '=%.0s' {1..100}; printf '\n' " "
 
 # Make run command (SERIAL)
 CMDSER="${FIELDOPT_BIN} "\
 "${DRIVER_PATH} ${OUTPUT_DIR} -r serial "\
-"-t5 --force -g ${GRID_PATH} -s ${DECK_PATH} "\
-"-e ${SCR_PATH} -v3"
+"-g ${GRID_PATH} -s ${DECK_PATH} "\
+"-e ${SCR_PATH} -f -v 3 -t 0"
 
-echo "CMDSER=${CMDSER}"
-echo ""
+printf '%s\n' "CMDSER=${CMDSER}"
 
 # eval ${CMDSER}
 eval ${CMDMPI}
