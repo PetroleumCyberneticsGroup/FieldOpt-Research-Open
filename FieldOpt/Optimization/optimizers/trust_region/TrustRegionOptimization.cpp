652e38065bb0c3c37586cf6be85f6c55e3420df0
Thiago Lima Silva <thiago.l.silva@ntnu.no>
2018-11-26 16:52:40 +0100

    New Class: TrustRegionOptimization

---
+++ FieldOpt/Optimization/optimizers/trust_region/TrustRegionOptimization.cpp
@@ -0,0 +1,93 @@
+/******************************************************************************
+   Created by thiagols on 26.11.18
+   Copyright (C) 2018 Thiago Lima Silva<thiagolims@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+#include <Utilities/verbosity.h>
+#include "Utilities/printer.hpp"
+#include "Utilities/stringhelpers.hpp"
+#include "TrustRegionOptimization.h"
+
+namespace Optimization {
+namespace Optimizers {
+
+TrustRegionOptimization::TrustRegionOptimization(Settings::Optimizer *settings,
+         Case *base_case,
+         Model::Properties::VariablePropertyContainer *variables,
+         Reservoir::Grid::Grid *grid,
+         Logger *logger,
+         CaseHandler *case_handler,
+         Constraints::ConstraintHandler *constraint_handler
+) : Optimizer(settings, base_case, variables, grid, logger, case_handler, constraint_handler) {
+    if (constraint_handler_->HasBoundaryConstraints()) {
+        lb_ = constraint_handler_->GetLowerBounds(base_case->GetRealVarIdVector());
+        ub_ = constraint_handler_->GetUpperBounds(base_case->GetRealVarIdVector());
+    }
+    else {
+        lb_.resize(base_case->GetRealVarIdVector().size());
+        ub_.resize(base_case->GetRealVarIdVector().size());
+        lb_.fill(settings->parameters().lower_bound);
+        ub_.fill(settings->parameters().upper_bound);
+    }
+
+    if (enable_logging_) { // Log base case
+        logger_->AddEntry(this);
+    }
+
+    // TODO: implement TR Optimization constructor.
+
+
+    if (enable_logging_) {
+        logger_->AddEntry(new ConfigurationSummary(this));
+    }
+}
+Optimization::Optimizer::TerminationCondition TrustRegionOptimization::IsFinished() {
+    TerminationCondition tc = NOT_FINISHED;
+    if (case_handler_->CasesBeingEvaluated().size() > 0)
+        return tc;
+    if (evaluated_cases_ > max_evaluations_)
+        tc = MAX_EVALS_REACHED;
+    if (tc != NOT_FINISHED) {
+        if (enable_logging_) {
+            logger_->AddEntry(this);
+        }
+    }
+    return tc;
+}
+void TrustRegionOptimization::handleEvaluatedCase(Case *c) {
+    // TODO: implement a handler for evaluated cases in TR optimization.
+    if (isImprovement(c)) {
+        updateTentativeBestCase(c);
+        Printer::ext_info("Found new tentative best case: " + Printer::num2str(c->objective_function_value()), "Optimization", "EGO");
+    }
+}
+void TrustRegionOptimization::iterate() {
+    // TODO: implement the main iteration for the TR optimization algorithm
+
+    if (enable_logging_) {
+        logger_->AddEntry(this);
+    }
+}
+Loggable::LogTarget TrustRegionOptimization::ConfigurationSummary::GetLogTarget() {
+    return LOG_SUMMARY;
+}
+QUuid TrustRegionOptimization::ConfigurationSummary::GetId() {
+    return QUuid(); // Null UUID
+}
+
+}
+}
---
+++ FieldOpt/Optimization/optimizers/trust_region/TrustRegionOptimization.h
@@ -0,0 +1,69 @@
+/******************************************************************************
+   Created by thiagols on 26.11.18
+   Copyright (C) 2018 Thiago Lima Silva<thiagolims@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+#ifndef FIELDOPT_TRUSTREGION_H
+#define FIELDOPT_TRUSTREGION_H
+
+#include "Optimization/optimizer.h"
+
+namespace Optimization {
+namespace Optimizers {
+
+
+/*!
+ * @brief This class is an implementation of a Trust Region (TR) algorithm for derivative-free optimization
+ *
+ * \todo Derivative-free Optimization algorithm
+ * \todo TR model
+ * \todo TR sub-problem optimization
+ * \todo Implement tests
+ */
+class TrustRegionOptimization : public Optimizer {
+ public:
+    TerminationCondition IsFinished() override;
+    TrustRegionOptimization(Settings::Optimizer *settings,
+                Case *base_case,
+                Model::Properties::VariablePropertyContainer *variables,
+                Reservoir::Grid::Grid *grid,
+                Logger *logger,
+                CaseHandler *case_handler=0,
+                Constraints::ConstraintHandler *constraint_handler=0
+    );
+
+ protected:
+    void handleEvaluatedCase(Case *c) override;
+    void iterate() override;
+
+ private:
+    VectorXd lb_, ub_; //!< Upper and lower bounds
+    Settings::Optimizer *settings_;
+
+    class ConfigurationSummary : public Loggable {
+    public:
+        ConfigurationSummary(TrustRegionOptimization *opt) { opt_ = opt; }
+        LogTarget GetLogTarget() override;
+        QUuid GetId() override;
+    private:
+        TrustRegionOptimization *opt_;
+    };
+};
+
+}
+}
+#endif //FIELDOPT_TRUSTREGIONOPTIMIZATION_H
