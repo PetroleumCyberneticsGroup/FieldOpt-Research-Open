f17e12d7714cd4081cc1230a89df823eb2d32e48
bellout <mathias.bellout@ntnu.no>
2018-12-08 13:33:58 +0100

    Find hashed vector component sequence + compare init points.

--- FieldOpt/Optimization/tests/optimizers/test_tr-dfo.cpp
+++ FieldOpt/Optimization/tests/optimizers/test_tr-dfo.cpp
@@ -15,6 +15,7 @@
 #include "Optimization/optimizers/trust_region/TrustRegionOptimization.h"
 
 #include <Eigen/Core>
+#include <Eigen/Dense>
 #include "Utilities/math.hpp"
 #include "Utilities/colors.hpp"
 #include "Utilities/stringhelpers.hpp"
@@ -42,7 +43,7 @@ namespace {
         VariablePropertyContainer *varcont_tr_dfo_probs_;
         TestResources::TrustRegionModelData tr_mdata;
 
-        void SetUpOptimizer(TestResources::TrustRegionModelData::prob prob,
+        void SetUpOptimizer(TestResources::TrustRegionModelData::prob &prob,
                             double (*tr_dfo_prob)(VectorXd xs)){
 
             VectorXd x0 = prob.xm.col(0);
@@ -77,14 +78,72 @@ namespace {
                     logger_);
         }
 
-        void FindVarSequence(TestResources::TrustRegionModelData::prob prob) {
+        void CheckSameX(VectorXd xa, VectorXd xb,
+                        vector<int> idx, double tol,
+                        std::string msg) {
 
-          auto varcont = varcont_tr_dfo_probs_;
-//
-//          for (int ii=0; ii < prob.xm)
-//
-//          varcont.
+          VectorXd xbr(xb.size());
+          for (int ii=0; ii<xb.rows(); ii++) {
+            xbr.row(ii) << xb(idx[ii]);
+          }
+          // cout << "xa: [" << xa.transpose() << "]" << endl; // Matlab tr model data
+          // cout << "xb: [" << xb.transpose() << "]" << endl; // FO tr model data
+          // cout << "xbr: [" << xbr.transpose() << "]" << endl; // FO tr model data (reordered)
+
+          ASSERT_TRUE(xa.isApprox(xbr,tol));
+
+          if( ~msg.empty() ) {
+            stringstream ss; ss << "[          ] " << FMAGENTA;
+            cout << ss.str() << msg.c_str() << " -> ok" << END << endl;
+          }
         }
+
+        void FindVarSequence(TestResources::TrustRegionModelData::prob &prob) {
+
+          // First vector (Eigen + std formats)
+          VectorXd va = prob.xm.col(0); // <- va: VectorXd
+          vector<double> v1;
+          v1.resize(va.size());
+          VectorXd::Map(&v1[0], va.size()) = va; // <- v1: std
+
+          // Second vector (Eigen + std formats)
+          // QHash<QUuid,double> vbu = varcont_tr_dfo_probs_->GetContinousVariableValues();
+          list<double> vbl = varcont_tr_dfo_probs_->GetContinousVariableValues().values().toStdList();
+          vector<double> v2{ std::begin(vbl), std::end(vbl) }; // <- v2: std
+          Eigen::VectorXd vb = Eigen::Map<VectorXd>(v2.data(), v2.size()); // <- vb: VectorXd
+
+          // Eigen approach
+          double tol = 1e-6;
+          VectorXi idxa(va.rows(),1);
+
+          for (int ii=0; ii<va.size(); ii++) {
+            for (int jj=0; jj<vb.size(); jj++) {
+              if ( norm( va(ii) - vb(jj) ) < tol ) {
+                idxa.row(ii) << jj;
+              } else {
+                continue;
+              }
+            }
+          }
+          // cout << "ia:" << endl << ia << endl;
+
+          // std approah
+          // vector<int> idx1; // <- defined as function param
+
+          for (int ii=0; ii<v2.size(); ii++) {
+
+            auto it =
+                find_if(v1.begin(), v1.end(), [&](const double& x) {
+                  return norm( x - v2[ii] ) < tol;
+                } );
+
+            prob.idx.push_back( (int)distance(v1.begin(), it) );
+          }
+          // cout << "idx:";
+          // for (int ii=0; ii<idx1.size(); ii++) { cout << idx1[ii] << " "; }
+          // cout << endl;
+
+        } // End of void FindVarSequence
 
         void RunnerSubs(TestResources::TrustRegionModelData::prob prob,
                         double (*tr_dfo_prob)(VectorXd xs)){
@@ -94,7 +153,7 @@ namespace {
 
             // Tentative best case should be equal to base case at this point
             CheckSameX(tr_dfo_->GetTentativeBestCase()->GetRealVarVector(),
-                       prob.xm.col(0), 1e-6, "Check init point the same");
+                       prob.xm.col(0), prob.idx, 1e-6, "Check init point the same");
 
             while (!tr_dfo_->IsFinished()) {
 
@@ -112,7 +171,6 @@ namespace {
                 cout << ss.str() << "x:" << next_case->GetRealVarVector().transpose() << END << endl;
                 cout << ss.str() << "f:" << next_case->objective_function_value() << END << endl;
 
-
                 // RUNNER CALL (FINISH)
                 tr_dfo_->SubmitEvaluatedCase(next_case);
 
@@ -121,15 +179,6 @@ namespace {
             cout << ss.str() << "----------------------" << END << endl;
             auto best_case = tr_dfo_->GetTentativeBestCase();
             cout << best_case->objective_function_value() << endl;
-        }
-
-        void CheckSameX(VectorXd xa, VectorXd xb, double tol, std::string msg) {
-
-          // cout << "xa: " << xa << endl;
-          // cout << "xb: " << xb << endl;
-          ASSERT_TRUE(xa.isApprox(xb,tol));
-          stringstream ss; ss << "[          ] " << FMAGENTA;
-          cout << ss.str() << msg.c_str() << " -> ok" << END << endl;
         }
 
     };
@@ -152,16 +201,16 @@ namespace {
         cout << FMAGENTA
              << "[CG.prob2  ] f = @(x) log1p(x(1)^2) + x(2)^2; x0=[2.0 2.0]"
              << END << endl;
-        SetUpOptimizer(tr_mdata.prob1, tr_dfo_prob2);
-        RunnerSubs(tr_mdata.prob1, tr_dfo_prob2);
+        SetUpOptimizer(tr_mdata.prob2, tr_dfo_prob2);
+        RunnerSubs(tr_mdata.prob2, tr_dfo_prob2);
     }
 
     TEST_F(TrustRegionTest, tr_dfo_prob3) {
         cout << FMAGENTA
              << "[CG.prob3  ] f = @(x) sin(pi*x(1)/12) * cos(pi*x(2)/16); x0=[0.0 0.0]"
              << END << endl;
-        SetUpOptimizer(tr_mdata.prob1, tr_dfo_prob3);
-        RunnerSubs(tr_mdata.prob1, tr_dfo_prob3);
+        SetUpOptimizer(tr_mdata.prob3, tr_dfo_prob3);
+        RunnerSubs(tr_mdata.prob3, tr_dfo_prob3);
     }
 
     TEST_F(TrustRegionTest, tr_dfo_initial_model_created) {
