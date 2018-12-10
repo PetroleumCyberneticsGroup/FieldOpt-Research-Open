26b7a0a3facad987773f64a814a9691acc83d9ed
bellout <mathias.bellout@ntnu.no>
2018-12-10 09:38:04 +0100

    Function to override 2nd point. Place print output into function.

--- FieldOpt/Optimization/tests/optimizers/test_tr-dfo.cpp
+++ FieldOpt/Optimization/tests/optimizers/test_tr-dfo.cpp
@@ -125,7 +125,7 @@ namespace {
               }
             }
           }
-          // cout << "ia:" << endl << ia << endl;
+          // cout << "ia:" << endl << ia << endl; // dbg
 
           // std approah
           // vector<int> idx1; // <- defined as function param
@@ -139,21 +139,51 @@ namespace {
 
             prob.idx.push_back( (int)distance(v1.begin(), it) );
           }
+          // dbg
           // cout << "idx:";
           // for (int ii=0; ii<idx1.size(); ii++) { cout << idx1[ii] << " "; }
           // cout << endl;
 
         } // End of void FindVarSequence
 
+        void PrintCaseData(Optimization::Case &c,
+                           TrustRegionOptimization &tr_dfo){
+
+          // PRINT CASE DATA (ID, X, F)
+          stringstream ss; ss << "[          ] " << FMAGENTA;
+          cout << ss.str()
+          << "---------------------------------------------------------" << END << endl;
+          cout << ss.str() << "Case id: " << c.GetId().toString().toStdString() << END << endl;
+          cout << ss.str() << "# initial points: " << tr_dfo.GetNumInitPoints() << END << endl;
+          cout << ss.str() << "x: [" << c.GetRealVarVector().transpose() << "]" << END << endl;
+          cout << ss.str() << "f: [" << c.objective_function_value() << "]" << END << endl;
+
+        }
+
+        void OverrideSecondPoint(TestResources::TrustRegionModelData::prob &prob,
+                                 Optimization::Case &c) {
+
+            VectorXd xbr(prob.xm.rows(),1);
+            for (int ii=0; ii<prob.xm.rows(); ii++) {
+              xbr.row(ii) << prob.xm(prob.idx[ii],1);
+            }
+
+            c.SetRealVarValues(xbr.col(0));
+            c.set_objective_function_value(prob.fm(0,1));
+        }
+
+
+
         void RunnerSubs(TestResources::TrustRegionModelData::prob prob,
                         double (*tr_dfo_prob)(VectorXd xs)){
 
             stringstream ss; ss << "[          ] " << FMAGENTA;
-            cout << ss.str() << "-------------------------------------------------------" << END << endl;
 
             // Tentative best case should be equal to base case at this point
+            PrintCaseData(*tr_dfo_->GetTentativeBestCase(), *tr_dfo_);
             CheckSameX(tr_dfo_->GetTentativeBestCase()->GetRealVarVector(),
-                       prob.xm.col(0), prob.idx, 1e-6, "Check init point the same");
+                       prob.xm.col(0), prob.idx, 1e-6,
+                       "Check 1st point equal");
 
             while (!tr_dfo_->IsFinished()) {
 
@@ -164,15 +194,18 @@ namespace {
                 next_case->set_objective_function_value(
                         tr_dfo_prob(next_case->GetRealVarVector()));
 
-                // PRINT CASE DATA (ID, X, F)
-                cout << ss.str() << "-------------------------------------------------------" << END << endl;
-                cout << ss.str() << "Case id:" << next_case->GetId().toString().toStdString() << END << endl;
-                cout << ss.str() << "# initial points: " << tr_dfo_->GetNumInitPoints() << END << endl;
-                cout << ss.str() << "x:" << next_case->GetRealVarVector().transpose() << END << endl;
-                cout << ss.str() << "f:" << next_case->objective_function_value() << END << endl;
+                if (tr_dfo_->GetNumInitPoints() == 1 ) {
+                  OverrideSecondPoint(prob, *next_case);
+                }
 
                 // RUNNER CALL (FINISH)
                 tr_dfo_->SubmitEvaluatedCase(next_case);
+
+                // PRINT CASE DATA (ID, X, F)
+                PrintCaseData(*next_case, *tr_dfo_);
+                CheckSameX(next_case->GetRealVarVector(),
+                           prob.xm.col(1), prob.idx, 1e-6,
+                           "Check 2nd point equal");
 
             }
 
@@ -191,7 +224,7 @@ namespace {
 
     TEST_F(TrustRegionTest, tr_dfo_prob1) {
         cout << FMAGENTA
-             << "[CG.prob1  ] f = @(x) (1 - x(1))^2; x0=[-1.2 2.0]"
+             << "[ CG.prob1 ] f = @(x) (1 - x(1))^2; x0=[-1.2 2.0]"
              << END << endl;
         SetUpOptimizer(tr_mdata.prob1, tr_dfo_prob1);
         RunnerSubs(tr_mdata.prob1, tr_dfo_prob1);
@@ -199,7 +232,7 @@ namespace {
 
     TEST_F(TrustRegionTest, tr_dfo_prob2) {
         cout << FMAGENTA
-             << "[CG.prob2  ] f = @(x) log1p(x(1)^2) + x(2)^2; x0=[2.0 2.0]"
+             << "[ CG.prob2 ] f = @(x) log1p(x(1)^2) + x(2)^2; x0=[2.0 2.0]"
              << END << endl;
         SetUpOptimizer(tr_mdata.prob2, tr_dfo_prob2);
         RunnerSubs(tr_mdata.prob2, tr_dfo_prob2);
@@ -207,7 +240,7 @@ namespace {
 
     TEST_F(TrustRegionTest, tr_dfo_prob3) {
         cout << FMAGENTA
-             << "[CG.prob3  ] f = @(x) sin(pi*x(1)/12) * cos(pi*x(2)/16); x0=[0.0 0.0]"
+             << "[ CG.prob3 ] f = @(x) sin(pi*x(1)/12) * cos(pi*x(2)/16); x0=[0.0 0.0]"
              << END << endl;
         SetUpOptimizer(tr_mdata.prob3, tr_dfo_prob3);
         RunnerSubs(tr_mdata.prob3, tr_dfo_prob3);
--- FieldOpt/Optimization/tests/optimizers/test_tr-model-data.hpp
+++ FieldOpt/Optimization/tests/optimizers/test_tr-model-data.hpp
@@ -10,11 +10,11 @@
 
  FieldOpt is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with FieldOpt. If not, see <http://www.gnu.org/licenses/>.
+ along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
 #ifndef FIELDOPT_TEST_TR_DFO_DATA_H
