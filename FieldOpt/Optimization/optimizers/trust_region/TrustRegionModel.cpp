6854479caca0ceb02750b21024f7b82a85574097
bellout <mathias.bellout@ntnu.no>
2019-02-19 17:07:01 +0100

    Added exit_flag = success; after used of improveModelNfp() to get algorithm moving.

--- FieldOpt/Optimization/optimizers/trust_region/TrustRegionModel.cpp
+++ FieldOpt/Optimization/optimizers/trust_region/TrustRegionModel.cpp
@@ -596,6 +596,8 @@ int TrustRegionModel::ensureImprovement() {
   if (!model_complete && (!model_old || !model_fl)) {
     //!<Calculate a new point to add>
     success = improveModelNfp(); //!<improve model>
+    exit_flag = success; // added by MB
+
     if (success) {
       exit_flag = 1;
     }
@@ -612,6 +614,8 @@ int TrustRegionModel::ensureImprovement() {
       if (!model_complete) {
         //!<Improve model>
         success = improveModelNfp();
+        exit_flag = success; // added by MB
+
       } else {
         //!<Replace point>
         success = chooseAndReplacePoint();
