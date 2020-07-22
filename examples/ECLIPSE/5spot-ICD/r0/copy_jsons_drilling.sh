82c46ee737b7f0ae864bfa017a7dec765c113c09
thiagols <thiago.l.silva@ntnu.no>
2020-07-22 15:04:39 +0200

    5spot ensemble
    -Uncertainty in the permeability (multiplier)
    -JSONs for ICD optimization (standalone and while drilling)

---
+++ examples/ECLIPSE/5spot-ICD/r0/copy_jsons_drilling.sh
@@ -0,0 +1,10 @@
+cp fo_driver_2_horz_icd_apps_drilling.json ../r1/
+cp fo_driver_2_horz_icd_apps_drilling.json ../r2/
+cp fo_driver_2_horz_icd_apps_drilling.json ../r3/
+cp fo_driver_2_horz_icd_apps_drilling.json ../r4/
+cp fo_driver_2_horz_icd_apps_drilling.json ../r5/
+cp fo_driver_2_horz_icd_apps_drilling.json ../r6/
+cp fo_driver_2_horz_icd_apps_drilling.json ../r7/
+cp fo_driver_2_horz_icd_apps_drilling.json ../r8/
+cp fo_driver_2_horz_icd_apps_drilling.json ../r9/
+
