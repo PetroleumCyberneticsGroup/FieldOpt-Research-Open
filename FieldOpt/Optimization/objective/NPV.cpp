4f1370fcf2f79b332123a6bf717a987f1ebcf1f5
bellout <mathias.bellout@ntnu.no>
2020-08-19 16:56:51 +0200

    NPV computation using report steps (match w/ e300) + NPVTimes.append() bug [report to dev].

--- FieldOpt/Optimization/objective/NPV.cpp
+++ FieldOpt/Optimization/objective/NPV.cpp
@@ -33,19 +33,21 @@ If not, see <http://www.gnu.org/licenses/>.
 #include "Model/wells/well.h"
 #include <Utilities/printer.hpp>
 
+namespace Optimization {
+namespace Objective {
+
 using std::cout;
 using std::endl;
 using std::fixed;
 using std::setprecision;
 using std::scientific;
 
-namespace Optimization {
-namespace Objective {
-
 using Printer::ext_warn;
 using Printer::ext_info;
 using Printer::info;
 using Printer::error;
+
+using namespace Simulation::Results;
 
 NPV::NPV(Settings::Optimizer *settings,
          Simulation::Results::Results *results,
@@ -69,7 +71,7 @@ NPV::NPV(Settings::Optimizer *settings,
     } else {
       comp->is_json_component = false;
       comp->property_name = settings->objective().NPV_sum.at(i).property;
-      comp->property = results_->GetPropertyKeyFromString(QString::fromStdString(comp->property_name));
+      comp->property = results_->GetPropertyKey(QString::fromStdString(comp->property_name));
     }
 
     comp->coefficient = settings->objective().NPV_sum.at(i).coefficient;
@@ -92,17 +94,20 @@ NPV::NPV(Settings::Optimizer *settings,
 double NPV::value() const {
   try {
     double value = 0;
-
     auto report_times = results_->GetValueVector(results_->Time);
-    auto NPV_times = new QList<double>;
+    auto report_steps = results_->GetValueVector(results_->Step);
+    // Bug? NPV_times needs to be cleared after each component
+    // auto NPV_times = new QList<double>;
+    auto NPV_times = new QList<QList<double>>;
     auto discount_factor_list = new QList<double>;
 
     for (int k = 0; k < components_->size(); ++k) {
-      if (components_->at(k)->is_json_component) {
-        continue;
-      }
+      auto curr_comp = components_->at(k);
+      auto NPV_times_temp = QList<double>();
+      
+      if (curr_comp->is_json_component) { continue; }
 
-      if (components_->at(k)->interval == "Yearly") {
+      if (curr_comp->interval == "Yearly") {
         double discount_factor;
         int j = 1;
         for (int i = 0; i < report_times.size(); i++) {
@@ -115,42 +120,71 @@ double NPV::value() const {
             continue;
           }
           if (std::fmod(report_times.at(i), 365) == 0) {
-            discount_factor = 1 / (1 * (pow(1 + components_->at(k)->discount, j - 1)));
+            discount_factor = 1 / (1 * (pow(1 + curr_comp->discount, j - 1)));
             discount_factor_list->append(discount_factor);
             j += 1;
-            NPV_times->append(i);
+            NPV_times_temp.append(i);
           }
         }
 
-      } else if (components_->at(k)->interval == "Monthly") {
+      } else if (curr_comp->interval == "Monthly") {
         double discount_factor;
-        double discount_rate = components_->at(k)->discount;
-        double monthly_discount = components_->at(k)->yearlyToMonthly(discount_rate);
+        double discount_rate = curr_comp->discount;
+        double monthly_discount = Optimization::Objective::NPV::Component::yearlyToMonthly(discount_rate);
         int j = 1;
         for (int i = 0; i < report_times.size(); i++) {
           if (std::fmod(report_times.at(i), 30) == 0) {
             discount_factor = 1 / (1 * (pow(1 + monthly_discount, j - 1)));
             discount_factor_list->append(discount_factor);
             j += 1;
-            NPV_times->append(i);
+            NPV_times_temp.append(i);
           }
         }
+
+      } else if (curr_comp->interval == "None" || curr_comp->interval == "ReportSteps") {
+        QVector<double> qVec = QVector<double>::fromStdVector(report_steps);
+        NPV_times_temp = QList<double>::fromVector(qVec);
       }
+      NPV_times->append(NPV_times_temp);
     }
 
     for (int i = 0; i < components_->size(); ++i) {
-      if (components_->at(i)->is_json_component) {
-        continue;
-      }
-      if (components_->at(i)->usediscountfactor) {
-        for (int j = 1; j < NPV_times->size(); ++j) {
-          auto prod_difference = components_->at(i)->resolveValueDiscount(results_, NPV_times->at(j))
-            - components_->at(i)->resolveValueDiscount(results_, NPV_times->at(j - 1));
-          value += prod_difference * components_->at(i)->coefficient * discount_factor_list->at(i);
+      auto curr_comp = components_->at(i);
+      if (curr_comp->is_json_component) { continue; }
+
+      if (curr_comp->usediscountfactor) {
+        for (int j = 1; j < NPV_times->at(i).size(); ++j) {
+          auto va = curr_comp->resolveValueDiscount(results_, NPV_times->at(i).at(j));
+          auto vb = curr_comp->resolveValueDiscount(results_, NPV_times->at(i).at(j - 1));
+          auto prod_difference = va - vb;
+          value += prod_difference * curr_comp->coefficient * discount_factor_list->at(i);
         }
-      } else if (!components_->at(i)->usediscountfactor) {
-        value += components_->at(i)->resolveValue(results_);
-        std::string prop_name = components_->at(i)->property_name;
+      } else {
+        Results::Property rate_prop = Results::Property::AuxProp;;
+
+        if (curr_comp->property == Results::Property::FieldOilProdTotal) {
+          rate_prop = Results::Property::FieldOilProdRate;
+
+        } else if (curr_comp->property == Results::Property::FieldGasProdTotal) {
+          rate_prop = Results::Property::FieldGasProdRate;
+
+        } else if (curr_comp->property == Results::Property::FieldWatProdTotal) {
+          rate_prop = Results::Property::FieldWatProdRate;
+
+        } else if (curr_comp->property == Results::Property::FieldWatInjTotal) {
+          rate_prop = Results::Property::FieldWatInjRate;
+        }
+
+        if (rate_prop != Results::Property::AuxProp) {
+          // NPV using rates over report time steps
+          for (int jj = 0; jj < NPV_times->at(i).size(); ++jj) {
+            value += curr_comp->coefficient * NPV_times->at(i).at(jj) * results_->GetValue(rate_prop, jj);
+          }
+        } else {
+          // NPV using endpoint values of cumulative vectors
+          value += curr_comp->resolveValue(results_);
+          std::string prop_name = curr_comp->property_name;
+        }
       }
     }
 
@@ -179,15 +213,14 @@ double NPV::value() const {
     }
     return value;
 
-  } catch (...) {
-    error("Failed to compute NPV. Returning 0.0");
+  } catch (std::exception const &ex) {
+    error("Failed to compute NPV: " + string(ex.what()) + "  Returning 0.0");
     return 0.0;
   }
 }
 
 double NPV::Component::resolveValue(Simulation::Results::Results *results) {
   return coefficient * results->GetValue(property);
-
 }
 
 double NPV::Component::resolveValueDiscount(Simulation::Results::Results *results,
