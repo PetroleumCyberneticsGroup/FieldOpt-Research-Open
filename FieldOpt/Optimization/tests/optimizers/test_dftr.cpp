bd94f94159443cb8cd28ad8c413a990beb8797eb
bellout <mathias.bellout@ntnu.no>
2021-04-01 02:42:00 +0200

    More detailed dftr-trdfo test comparison.

--- FieldOpt/Optimization/tests/optimizers/test_dftr.cpp
+++ FieldOpt/Optimization/tests/optimizers/test_dftr.cpp
@@ -140,85 +140,78 @@ class DFTRTest : public ::testing::Test,
                   double (*tr_dfo_prob)(VectorXd xs)){
     stringstream ss; ss << "[          ] " << FMAGENTA;
     double tol = 1e-06;
-    int p_count = 0;
+    int p_count_dftr = 0, p_count_trdfo = 0;
 
-    VectorXd XA, XB;
-    double FA, FB;
-    bool DX;
+    std::vector<VectorXd> vX_dftr, vX_trdfo, vXA, vXB;
+    std::vector<double> vF_dftr, vF_trdfo, vFA, vFB;
+    bool DX = false, DR = false, DF = false;
+    std::vector<string> lbl = { "A", "B" };
+    VectorXd data;
 
-    // Start opt loop --------------------------------------
-    while (tr_dfo_->IsFinished() == TC_NOT_FIN_) {
-
-      auto nxt_cs_dfo = tr_dfo_->GetCaseForEvaluation();
+    // Start DFTR loop ------------------------------------
+    while (dftr_->IsFinished() == TC_NOT_FIN_) {
       auto nxt_cs_dftr = dftr_->GetCaseForEvaluation();
 
-      while (nxt_cs_dfo == nullptr) {
-        if (tr_dfo_->IsFinished()) {
-          break;
-        } else {
-          nxt_cs_dfo = tr_dfo_->GetCaseForEvaluation();
-        }
-      }
-
       while (nxt_cs_dftr == nullptr) {
-        if (dftr_->IsFinished()) {
-          break;
-        } else {
-          nxt_cs_dftr = dftr_->GetCaseForEvaluation();
-        }
+        if (dftr_->IsFinished()) { break; }
+        else { nxt_cs_dftr = dftr_->GetCaseForEvaluation(); }
       }
-
-      if (tr_dfo_->IsFinished() || dftr_->IsFinished()) {
-        break;
-      }
-
-      // Compute obj.function value for case
-      nxt_cs_dfo->set_objf_value(
-        tr_dfo_prob(nxt_cs_dfo->GetRealVarVector()));
+      if (dftr_->IsFinished()) { break; }
 
       nxt_cs_dftr->set_objf_value(
         tr_dfo_prob(nxt_cs_dftr->GetRealVarVector()));
 
       // Override 2nd point
-      if (p_count == 1 && prob.xm.cols() > 1) {
-        TestResources::OverrideSecondPoint(prob, *nxt_cs_dfo);
+      if (p_count_dftr == 1 && prob.xm.cols() > 1) {
         TestResources::OverrideSecondPoint(prob, *nxt_cs_dftr);
       }
 
       // Finish Runner
-      tr_dfo_->SubmitEvaluatedCase(nxt_cs_dfo);
       dftr_->SubmitEvaluatedCase(nxt_cs_dftr);
+      p_count_dftr++;
 
-      // Tests
-      if (tr_dfo_->getTrustRegionModel()->isInitialized()
-        && dftr_->getTRMod()->isModInit()) {
-        XA = tr_dfo_->getTrustRegionModel()->getCurrentPoint();
-        XB = dftr_->getTRMod()->getCurrPt();
-
-        DX = ((XA.array() - XB.array()).abs() < eps).all();
-        // EXPECT_TRUE(DX);
-
-        FA = tr_dfo_->getTrustRegionModel()->getCurrentFval();
-        FB = dftr_->getTRMod()->getCurrFval();
-        // EXPECT_NEAR(FA, FB, eps);
-
-        if (!DX || abs(FA-FB) > eps) {
-          stringstream so;
-          so << setw(12) << scientific << right << setprecision(6)
-             << FYELLOW << "---------------------------------------------" << endl
-             << "TR_DFO: x* = " << XA.transpose()
-             << "  |  TR_DFO: f* = " << FA << endl
-             << "DFTR:   x* = " << XB.transpose()
-             << "  |  DFTR:   f* = " << FB << AEND << endl;
-          cout << so.str();
-        }
+      if (dftr_->getTRMod()->isModInit()) {
+        data.conservativeResize(dftr_->getTRMod()->getCurrPt().rows()+1, 1);
+        data << dftr_->getTRMod()->getCurrPt(), dftr_->getTRMod()->getRad();
+        vX_dftr.push_back(data);
+        vF_dftr.push_back(dftr_->getTRMod()->getCurrFval());
       }
-
-      p_count++;
     }
 
+    // Start TRDFO loop ------------------------------------
+    while (tr_dfo_->IsFinished() == TC_NOT_FIN_) {
+      auto nxt_cs_dfo = tr_dfo_->GetCaseForEvaluation();
+
+      while (nxt_cs_dfo == nullptr) {
+        if (tr_dfo_->IsFinished()) { break; }
+        else { nxt_cs_dfo = tr_dfo_->GetCaseForEvaluation(); }
+      }
+      if (tr_dfo_->IsFinished()) { break; }
+
+      // Compute obj.function value for case
+      nxt_cs_dfo->set_objf_value(
+        tr_dfo_prob(nxt_cs_dfo->GetRealVarVector()));
+
+      // Override 2nd point
+      if (p_count_trdfo == 1 && prob.xm.cols() > 1) {
+        TestResources::OverrideSecondPoint(prob, *nxt_cs_dfo);
+      }
+
+      // Finish Runner
+      tr_dfo_->SubmitEvaluatedCase(nxt_cs_dfo);
+      p_count_trdfo++;
+
+      if (tr_dfo_->getTrustRegionModel()->isInitialized()) {
+        data.conservativeResize(tr_dfo_->getTrustRegionModel()->getCurrentPoint().rows()+1, 1);
+        data << tr_dfo_->getTrustRegionModel()->getCurrentPoint(), tr_dfo_->getTrustRegionModel()->getRadius();
+        vX_trdfo.push_back(data);
+        vF_trdfo.push_back(tr_dfo_->getTrustRegionModel()->getCurrentFval());
+      }
+    }
+
+    // Start comparison ------------------------------------
     stringstream sx;
-    string cc_trdfo, cc_dftr;
+    string ccA, ccB, cc_trdfo, cc_dftr;
 
     if (tr_dfo_->IsFinished() == TC_OPT_CRIT) {
       cc_trdfo = "OPT_CRITERIA_REACHED";
@@ -240,79 +233,121 @@ class DFTRTest : public ::testing::Test,
       cc_dftr += "MAX_ITERS_REACHED";
     }
 
+    if (vX_dftr.size() >= vX_trdfo.size()) {
+      vXA = vX_dftr;
+      vXB = vX_trdfo;
+      vFA = vF_dftr;
+      vFB = vF_trdfo;
+      lbl = { "DFTR__", "TR-DFO" };
+      ccA = cc_dftr;
+      ccB = cc_trdfo;
+    } else {
+      vXB = vX_dftr;
+      vXA = vX_trdfo;
+      vFB = vF_dftr;
+      vFA = vF_trdfo;
+      lbl = { "TR-DFO", "DFTR__" };
+      ccB = cc_dftr;
+      ccA = cc_trdfo;
+    }
+
+    stringstream so;
+    so << FYELLOW << setw(12) << scientific << right << setprecision(6);
+    so << "" << endl;
+
+    for (int ii=0; ii < vXA.size(); ii++) {
+      so << lbl[0] << " x = " << vXA[ii].transpose() << " | f = " << vFA[ii] << endl;
+      if(ii < vXB.size()) {
+        DX = ((vXA[ii].head(vXA[ii].rows()-1) - vXB[ii].head(vXB[ii].rows()-1)).array().abs() < 2*eps).all();
+        DR = ((vXA[ii].tail(1) - vXB[ii].tail(1)).array().abs() < 2*eps).all();
+        DF = (abs(vFA[ii] - vFB[ii]) < 2*eps);
+        if (DX && DR && DF) {
+          so << lbl[1] << " x = " << vXB[ii].transpose() << " | f = " << vFB[ii];
+          so << " -> DX=" << DX << " -- DF=" << DF << " -- DR=" << DR << endl;
+        } else {
+          so << FRED << lbl[1] << " x = " << vXB[ii].transpose() << " | f = " << vFB[ii] << AEND;
+          if (!DX) { so << FRED << " -> DX=" << DX << AEND; } else { so << " -> DX=" << DX; }
+          if (!DF) { so << FRED << " -- DF=" << DF << AEND; } else { so << " -> DF=" << DF; }
+          if (!DR) { so << FRED << " -- DR=" << DR << AEND; } else { so << " -> DR=" << DR; }
+          so << endl;
+        }
+      }
+      so << AEND;
+    }
+    cout << so.str();
+
     EXPECT_TRUE(DX);
-    EXPECT_NEAR(FA, FB, eps);
+    EXPECT_TRUE(DF);
 
     sx << setw(12) << scientific << right << setprecision(6)
        << FYELLOW << "---------------------------------------------" << endl
-       << "TR_DFO: x* = " << XA.transpose()
-       << "  |  TR_DFO: f* = " << FA << endl
-       << "TR_DFO: tc: " << cc_trdfo.c_str() << endl
-       << "DFTR:   x* = " << XB.transpose()
-       << "  |  DFTR:   f* = " << FB << endl
-       << "DFTR:   tc: " << cc_dftr.c_str() << AEND << endl;
+       << lbl[0] << ": x* = " << vXA.back().transpose() << "  |  f* = " << vFA.back() << endl
+       << "tc: " << ccA << endl
+       << lbl[1] << ": x* = " << vXB.back().transpose() << "  |  f* = " << vFB.back() << endl
+       << "tc: " << ccB << endl;
+    sx << AEND;
     cout << sx.str();
 
     return true;
   }
 };
 
-// TEST_F(DFTRTest, trHS1) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ HS1 ] "
-//        << " 100*pow(x(1) - pow(x(0), 2), 2) + pow(1 - x(0), 2)" << END << endl;
-//
-//   SetUpOptimizers(tr_mdata.prob_hs1, hs1);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob_hs1, hs1));
-// }
-//
-// TEST_F(DFTRTest, Prob1) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob1 ] "
-//        << " f = @(x) (1 - x(0))^2; x0=[-1.2 2.0]" << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob1, tr_dfo_prob1);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob1, tr_dfo_prob1));
-// }
-//
-// TEST_F(DFTRTest, trDfoProb2) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob2 ] "
-//        << "f = @(x) log1p(x(0)^2) + x(1)^2; x0=[2.0 2.0]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob2, tr_dfo_prob2);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob2, tr_dfo_prob2));
-// }
-//
-// TEST_F(DFTRTest, trDfoProb3) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob3 ] "
-//        << "f = @(x) sin(pi*x(0)/12) * cos(pi*x(1)/16); x0=[0.0 0.0]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob3, tr_dfo_prob3);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob3, tr_dfo_prob3));
-// }
-//
-// TEST_F(DFTRTest, trDfoProb4) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob4 ] "
-//        << "f = @(x) 0.01*(x(1) - 1)^2 + (x(2) - x(1)^2)^2; x0=[2.0 2.0 2.0]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob4, tr_dfo_prob4);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob4, tr_dfo_prob4));
-// }
+TEST_F(DFTRTest, trHS1) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ HS1 ] "
+       << " 100*pow(x(1) - pow(x(0), 2), 2) + pow(1 - x(0), 2)" << END << endl;
+
+  SetUpOptimizers(tr_mdata.prob_hs1, hs1);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob_hs1, hs1));
+}
+
+TEST_F(DFTRTest, Prob1) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob1 ] "
+       << " f = @(x) (1 - x(0))^2; x0=[-1.2 2.0]" << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob1, tr_dfo_prob1);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob1, tr_dfo_prob1));
+}
+
+TEST_F(DFTRTest, trDfoProb2) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob2 ] "
+       << "f = @(x) log1p(x(0)^2) + x(1)^2; x0=[2.0 2.0]"
+       << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob2, tr_dfo_prob2);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob2, tr_dfo_prob2));
+}
+
+TEST_F(DFTRTest, trDfoProb3) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob3 ] "
+       << "f = @(x) sin(pi*x(0)/12) * cos(pi*x(1)/16); x0=[0.0 0.0]"
+       << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob3, tr_dfo_prob3);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob3, tr_dfo_prob3));
+}
+
+TEST_F(DFTRTest, trDfoProb4) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob4 ] "
+       << "f = @(x) 0.01*(x(1) - 1)^2 + (x(2) - x(1)^2)^2; x0=[2.0 2.0 2.0]"
+       << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob4, tr_dfo_prob4);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob4, tr_dfo_prob4));
+}
 
 TEST_F(DFTRTest, trDfoProb5) {
   cout << endl << FMAGENTA << "[          ] =============="
@@ -326,79 +361,79 @@ TEST_F(DFTRTest, trDfoProb5) {
   EXPECT_TRUE(RunnerSubs(tr_mdata.prob5, tr_dfo_prob5));
 }
 
-// TEST_F(DFTRTest, trDfoProb6) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob6 ] "
-//        << "f = @(x) (x(0) + x(1))^2 + (x(1) + x(2))^2; x0=[-4.0 1.0 1.0]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob6, tr_dfo_prob6);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob6, tr_dfo_prob6));
-// }
+TEST_F(DFTRTest, trDfoProb6) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob6 ] "
+       << "f = @(x) (x(0) + x(1))^2 + (x(1) + x(2))^2; x0=[-4.0 1.0 1.0]"
+       << END << endl;
 
-// TEST_F(DFTRTest, trDfoProb7) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob7 ] "
-//        << "f = @(x) log1p(x(0)^2) + log1p((x(0) - x(1))^2) " << endl
-//        << " + log1p((x(1) - x(2))^2) + log1p((x(2) - x(3))^2); "
-//           "x0=[2.0 2.0 2.0 2.0]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob7, tr_dfo_prob7);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob7, tr_dfo_prob7));
-// }
-//
-// TEST_F(DFTRTest, trDfoProb8) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob8 ] "
-//        << "f = @(x) (x(0)*x(1)*x(2)*x(3))^2; x0=[0.8 0.8 0.8 0.8]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob8, tr_dfo_prob8);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob8, tr_dfo_prob8));
-// }
-//
-// TEST_F(DFTRTest, trDfoProb9) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob9 ] "
-//        << "f = @(x) (x(0)-1)^2 + (x(1)-2)^2 + (x(2)-3)^2 + (x(3)-4)^2; x0=[1.0 1.0 1.0 1.0]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob9, tr_dfo_prob9);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob9, tr_dfo_prob9));
-// }
-//
-// TEST_F(DFTRTest, trDfoProb10) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob10 ] "
-//        << "f = @(x) (x(1) - x(2))^2 + (x(2) - x(3))^2 + " << endl
-//        << "(x(2) - x(3))^4 + (x(3) - x(4))^4; x0=[2.0 sqrt(2) -1.0 2-sqrt(2) 0.5]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob10, tr_dfo_prob10);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob10, tr_dfo_prob10));
-// }
-//
-// TEST_F(DFTRTest, trDfoProb11) {
-//   cout << endl << FMAGENTA << "[          ] =============="
-//        << "=========================================== " << endl
-//        << "[ CG.prob11 ] "
-//        << "f = @(x) sum(2*x./(x.*x + 1));; x0=[1.0 1.0 1.0 1.0]"
-//        << END << endl;
-//
-//   // -------------------------------------------------------
-//   SetUpOptimizers(tr_mdata.prob11, tr_dfo_prob11);
-//   EXPECT_TRUE(RunnerSubs(tr_mdata.prob11, tr_dfo_prob11));
-// }
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob6, tr_dfo_prob6);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob6, tr_dfo_prob6));
+}
+
+TEST_F(DFTRTest, trDfoProb7) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob7 ] "
+       << "f = @(x) log1p(x(0)^2) + log1p((x(0) - x(1))^2) " << endl
+       << " + log1p((x(1) - x(2))^2) + log1p((x(2) - x(3))^2); "
+          "x0=[2.0 2.0 2.0 2.0]"
+       << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob7, tr_dfo_prob7);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob7, tr_dfo_prob7));
+}
+
+TEST_F(DFTRTest, trDfoProb8) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob8 ] "
+       << "f = @(x) (x(0)*x(1)*x(2)*x(3))^2; x0=[0.8 0.8 0.8 0.8]"
+       << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob8, tr_dfo_prob8);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob8, tr_dfo_prob8));
+}
+
+TEST_F(DFTRTest, trDfoProb9) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob9 ] "
+       << "f = @(x) (x(0)-1)^2 + (x(1)-2)^2 + (x(2)-3)^2 + (x(3)-4)^2; x0=[1.0 1.0 1.0 1.0]"
+       << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob9, tr_dfo_prob9);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob9, tr_dfo_prob9));
+}
+
+TEST_F(DFTRTest, trDfoProb10) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob10 ] "
+       << "f = @(x) (x(1) - x(2))^2 + (x(2) - x(3))^2 + " << endl
+       << "(x(2) - x(3))^4 + (x(3) - x(4))^4; x0=[2.0 sqrt(2) -1.0 2-sqrt(2) 0.5]"
+       << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob10, tr_dfo_prob10);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob10, tr_dfo_prob10));
+}
+
+TEST_F(DFTRTest, trDfoProb11) {
+  cout << endl << FMAGENTA << "[          ] =============="
+       << "=========================================== " << endl
+       << "[ CG.prob11 ] "
+       << "f = @(x) sum(2*x./(x.*x + 1));; x0=[1.0 1.0 1.0 1.0]"
+       << END << endl;
+
+  // -------------------------------------------------------
+  SetUpOptimizers(tr_mdata.prob11, tr_dfo_prob11);
+  EXPECT_TRUE(RunnerSubs(tr_mdata.prob11, tr_dfo_prob11));
+}
 
 }
